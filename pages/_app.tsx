28e7b56ead720dcaa18830653dd7f03d03c48dab
sujin lee <jmakeaz@gmail.com>
2022-03-13 00:23:42 +0900

    절대 경로 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
-import theme from "../styles/theme";
+import theme from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
